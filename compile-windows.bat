f07a60796acf7f84b05a8bee53708aa96c6b71d3
marius851000 <marius851000@gmail.com>
2021-05-06 20:00:11 +0200

    Update compile-windows.bat

--- compile-windows.bat
+++ compile-windows.bat
@@ -1,2 +1,2 @@
 
-win64/ndstool.exe -c rom.nds -9 rom/arm9.bin -7 rom/arm7.bin -d rom/data -y rom/overlays -y9 rom/y9.bin -y7 rom/y7.bin -t rom/banner.bin -h rom/header.bin
+win64\ndstool.exe -c rom.nds -9 rom/arm9.bin -7 rom/arm7.bin -d rom/data -y rom/overlays -y9 rom/y9.bin -y7 rom/y7.bin -t rom/banner.bin -h rom/header.bin
